d8547174a6cffddaa0f98f596b81e900cd828fd3
Aashish John <aashishjohn25@gmail.com>
2026-06-02 22:28:01 +0000

    feat: add routing

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { NumbersClient } from './lib/NumbersClient';
 export * from './lib/lookup/LookupClient';
 export * from './lib/lookup/Request';
+export * from './lib/routing/RoutingClient';
 export default NumbersClient;
---
+++ src/lib/routing/RoutingClient.ts
@@ -0,0 +1,53 @@
+import request from 'superagent';
+
+import { RequestFactoryWrapper, RequestFactory } from '../NumbersClient';
+
+export const ROUTING_PATH = '/routing/get-number-for-contact';
+
+type RoutingClientConstructor = {
+  requestWrapper: RequestFactoryWrapper;
+  routingPath?: string;
+};
+
+interface GetNumberForContactArgs {
+  toNumber: string;
+  profileId: string;
+  contactZipCode: string;
+}
+
+interface GetNumberForContactResult {
+  fromNumber: string;
+}
+
+class RoutingClient {
+  _requestFactory: RequestFactory = null;
+  routingPath: string = ROUTING_PATH;
+
+  constructor(options: RoutingClientConstructor) {
+    const { requestWrapper, routingPath } = options;
+
+    if (!requestWrapper) {
+      throw new Error('Parameter `requestWrapper` required in constructor');
+    }
+
+    if (routingPath) {
+      this.routingPath = routingPath;
+    }
+
+    this._requestFactory = requestWrapper(this.routingPath);
+  }
+
+  getNumberForContact = async (
+    args: GetNumberForContactArgs
+  ): Promise<GetNumberForContactResult> => {
+    const { toNumber, profileId, contactZipCode } = args;
+    const response = await this._requestFactory().query({
+      to_number: toNumber,
+      profile_id: profileId,
+      contact_zip_code: contactZipCode,
+    });
+    return { fromNumber: response.body.from_number };
+  };
+}
+
+export { RoutingClient };
